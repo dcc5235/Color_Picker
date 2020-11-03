30a133cc64c9606e0d9e3820931a5c3854dcf947
Dany <dany@Danys-MacBook-Pro.local>
2020-11-03 10:23:45 -0600

    loop over box container with colors

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ const colors = [
   'indigo',
   'violet'
 ];
+
+const container = document.querySelector('#boxes');
+
+for(let color of colors) {
+  const box = document.createElement('div');
+  box.style.backgroundColor = color;
+  box.classList.add('box');
+  container.appendChild(box);
+}
